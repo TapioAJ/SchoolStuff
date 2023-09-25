246eddecf68e59ac149980ab6d980ab8a4ea0b73
TapioAJ <104069226+TapioAJ@users.noreply.github.com>
2023-09-25 12:57:57 +0300

    Create test.java

---
+++ TwoPoints/tp/src/main/java/com/twopoints/test.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DistanceCalculatorTest {
+
+    @Test
+    public void testCalculateDistance() {
+        double x1 = 3;
+        double y1 = 4;
+        double x2 = 7;
+        double y2 = 1;
+
+        double distance = Calculator.calculateDistance(x1, y1, x2, y2);
+
+        assertEquals(distance, 5, 0.001);
+    }
